73b709d0cab2153be53b4209285ba193f3ac0339
Yarden Hochman <yyyarden@gmail.com>
2018-02-07 16:52:52 -0500

    added image

--- src/main_display.js
+++ src/main_display.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default class MainDisplay extends React.Component {
   render() {
