6223fb20b45026ca0453e13e55a13f44da28a792
Yarden Hochman <yyyarden@gmail.com>
2018-02-07 16:46:07 -0500

    adding db

--- App.js
+++ App.js
@@ -5,12 +5,10 @@ import Header from './src/header';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
+    return <View style={styles.container}>
         <Header />
-        <MainDisplay />
-      </View>
-    );
+        <MainDisplay poses={poses} />
+      </View>;
   }
 }
 
@@ -22,3 +20,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+const poses = [
+  {
+    name: 'Moon Bridge',
+    type: 'L-Base',
+    difficulty: 'Intermediate',
+    number_of_people: '2 Person',
+    img: 'https://www.acropedia.org/wp-content/uploads/2017/01/3.jpg',
+  },
+  {
+    name: 'Double Throne',
+    type: 'L-Base',
+    difficulty: 'Intermediate',
+    number_of_people: '3 Person',
+    img: 'https://www.acropedia.org/wp-content/uploads/2016/09/double-throne.jpg',
+  },
+];
---
+++ db.js
@@ -0,0 +1,17 @@
+
+const poses = [
+  {
+    name: 'Moon Bridge',
+    type: 'L-Base',
+    difficulty: 'Intermediate',
+    number_of_people: '2 Person',
+    img: 'https://www.acropedia.org/wp-content/uploads/2017/01/3.jpg',
+  },
+  {
+    name: 'Double Throne',
+    type: 'L-Base',
+    difficulty: 'Intermediate',
+    number_of_people: '3 Person',
+    img: 'https://www.acropedia.org/wp-content/uploads/2016/09/double-throne.jpg',
+  },
+];
