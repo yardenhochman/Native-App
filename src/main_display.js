d2ae8179ca9185e0cffd7f818ea17bab4716a0e8
Yarden Hochman <yyyarden@gmail.com>
2018-02-07 16:51:18 -0500

    added image

--- src/main_display.js
+++ src/main_display.js
@@ -3,11 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class MainDisplay extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
+    return <View style={styles.container}>
         <Text>{this.props.poses[0].name} </Text>
-      </View>
-    );
+        <Image source={{ uri: `${this.props.poses[0].img}` }} style={{ width: 40, height: 40 }} />
+      </View>;
   }
 }
 
